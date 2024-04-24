113511b9037aff393193baf17d3f2c6e6e58141d
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-04-24 19:38:02 -0300

    Update index.tsx

--- src/components/Pages/Login/index.tsx
+++ src/components/Pages/Login/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 export const LoginContainer = () => {
   return (
-    <div className="md:p-[2rem] md:items-center justify-center flex w-full h-[100vh] min-h-[100vh] p-[2rem] bg-[#00000000] overflow-y-auto">
+    <div className="md:p-[2rem] md:items-center justify-center flex w-full h-[100vh] min-h-[100vh] p-[1rem] bg-[#00000000] overflow-y-auto">
       <ScreanContainer />
       <Form />
     </div>
