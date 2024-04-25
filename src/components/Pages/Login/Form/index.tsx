7b6778db24ec23a278cdf0bc323a967a4084a5bf
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-04-25 09:22:59 -0300

    fix: configurações no css

--- src/components/Pages/Login/Form/index.tsx
+++ src/components/Pages/Login/Form/index.tsx
@@ -15,49 +15,52 @@ export const Form = () => {
         </div>
         <hr />
       </div>
-      <div className="">
+      <div>
         <div className="formContentTitle">
           <figure className="imageSaiaDetail">
             <img src={logoSaiaGrafico} alt="" />
           </figure>
           <h2 className="titleForm">Faça login na plataforma</h2>
         </div>
-        <form className="w-full flex flex-col gap-[2.4rem] items-center animationForm">
-          <div className="flex items-center justify-center w-full gap-2 border-b border-black/20">
-            <label className="pl-2" htmlFor="userName">
-              <User size={30} color="#0000004d" weight="bold" />
-            </label>
-            <input
-              className="flex-1 p-3 outline-none placeholder:font-bold placeholder:text-black/30"
-              type="text"
-              name="userName"
-              id="userName"
-              placeholder="Nome de Usuário"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-center w-full gap-2 border-b border-black/20">
-            <label className="pl-2" htmlFor="userPassword">
-              <Key size={30} color="#0000004d" weight="bold" />
-            </label>
-            <input
-              className="flex-1 p-3 outline-none placeholder:font-bold placeholder:text-black/30"
-              type="password"
-              name="userPassword"
-              placeholder="Senha"
-              id="userPassword"
-              required
-            />
-            <label className="pr-2">
-              <Eye size={30} color="#0000004d" weight="bold" />
-            </label>
-          </div>
-          <button className="bg-[#63C0D4] w-full p-[18px] rounded-xl text-xs md:mb-[10rem] mb-[4rem] text-white font-bold transition-all ease-out duration-[0.5s] hover:bg-[#4d93a3]">
-            ENVIAR
-          </button>
-        </form>
+
+        <div className="w-full lg:w-[450px]">
+          <form className="w-full flex flex-col gap-[2rem] items-center animationForm">
+            <div className="flex items-center justify-center w-full gap-2 border-b border-black/20">
+              <label className="pl-2" htmlFor="userName">
+                <User size={30} color="#0000004d" weight="bold" />
+              </label>
+              <input
+                className="flex-1 p-3 outline-none placeholder:font-bold placeholder:text-black/30"
+                type="text"
+                name="userName"
+                id="userName"
+                placeholder="Nome de Usuário"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-center w-full gap-2 border-b border-black/20">
+              <label className="pl-2" htmlFor="userPassword">
+                <Key size={30} color="#0000004d" weight="bold" />
+              </label>
+              <input
+                className="flex-1 p-3 outline-none placeholder:font-bold placeholder:text-black/30"
+                type="password"
+                name="userPassword"
+                placeholder="Senha"
+                id="userPassword"
+                required
+              />
+              <label className="pr-2">
+                <Eye size={30} color="#0000004d" weight="bold" />
+              </label>
+            </div>
+            <button className="bg-[#63C0D4] w-full p-[18px] rounded-xl text-xs  text-white font-bold transition-all ease-out duration-[0.5s] hover:bg-[#4d93a3]">
+              ENVIAR
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="logoUFPA">
+      <div className="logoUFPA ">
         <a target="_blank" href="http://ufpa.br">
           <img src={logoUfpa} alt="Logo da UFPA" />
         </a>
