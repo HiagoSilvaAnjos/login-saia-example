1a8a96447535266f0664abcd02c62ad93badbb93
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-04-26 17:19:09 -0300

    feat: logando o usuário - salvando cookies e permissões

--- src/components/Pages/Login/Form/index.tsx
+++ src/components/Pages/Login/Form/index.tsx
@@ -6,23 +6,71 @@ import "./index.css";
 
 import { Eye, EyeSlash, Key, User } from "@phosphor-icons/react";
 
-import { useState } from "react";
-
-// import { useNavigate } from "react-router-dom";
+import { FormEvent, useState } from "react";
 
 export const Form = () => {
-  // const navigate = useNavigate();
-
   const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
-  console.log(userName);
-  console.log(userPassword);
+  const [status200, setStatus200] = useState(false);
+  const [status400, setStatus400] = useState(false);
 
   const [styleInputPassword, setStyleInputPassword] = useState(false);
   const [styleInputUserName, setStyleInputUserName] = useState(false);
 
   const [showPassword, setShowPassword] = useState(false);
+
+  // enviar o corpo da requisição  contendo os dados do formulário
+  const body = new URLSearchParams();
+  body.append("username", userName);
+  body.append("password", userPassword);
+
+  const handleEventSubmitForm = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      // Configurando o fetch
+      const configFetch = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Accept: "application/json",
+        },
+        body: body,
+      };
+
+      const response = await fetch(
+        `https://katudv.com/api/saia-dev/login`,
+        configFetch
+      );
+
+      const responseJSON = await response.json();
+
+      console.log(responseJSON);
+
+      if (response.status === 200) {
+        setStatus200(true);
+
+        setTimeout(() => {
+          // Salvando os tokens de acesso nos cookies
+          const token = responseJSON.access_token;
+          const permissoes = responseJSON.permissoes;
+          const username = responseJSON.username;
+          document.cookie = `permissoes=${permissoes};max-age=5400`;
+          document.cookie = `username=${username};max-age=5400`;
+          document.cookie = `token=${token};max-age=5400`;
+          window.location.href = "http://katudv.com/projeto/saia-dev/dashboard";
+        }, 1000);
+      } else {
+        setStatus400(true);
+        setTimeout(() => {
+          setStatus400(false);
+        }, 1000);
+      }
+    } catch (error) {
+      console.log("error");
+    }
+  };
 
   return (
     <div className="loginFormContainer">
@@ -42,17 +90,19 @@ export const Form = () => {
         </div>
 
         <div className="form-container">
-          <form className="form-content">
+          <form onSubmit={handleEventSubmitForm} className="form-content">
             <div
-              className={`form-input-container ${
-                styleInputUserName ? "input-focus" : ""
-              }`}
+              className={`form-input-container 
+              ${styleInputUserName ? "input-focus" : ""} 
+              ${status200 ? "input-focus-success" : ""} 
+              ${status400 ? "input-focus-error" : ""}`}
             >
               <label className="pl-2" htmlFor="userName">
                 <User
-                  className={
-                    styleInputUserName ? "icon-user-focus" : "icon-user-blur"
-                  }
+                  className={`
+                  ${styleInputUserName ? "icon-user-focus" : "icon-user-blur"} 
+                  ${status200 ? "icon-user-focus-success" : ""} 
+                  ${status400 ? "icon-user-focus-error" : ""}`}
                   size={30}
                   weight="bold"
                 />
@@ -70,15 +120,17 @@ export const Form = () => {
               />
             </div>
             <div
-              className={`form-input-container ${
-                styleInputPassword ? "input-focus" : ""
-              }`}
+              className={`form-input-container 
+              ${styleInputPassword ? "input-focus" : ""} 
+              ${status200 ? "input-focus-success" : ""} 
+              ${status400 ? "input-focus-error" : ""}`}
             >
               <label className="pl-2" htmlFor="userPassword">
                 <Key
-                  className={
-                    styleInputPassword ? "icon-user-focus" : "icon-user-blur"
-                  }
+                  className={`
+                  ${styleInputPassword ? "icon-user-focus" : "icon-user-blur"} 
+                  ${status200 ? "icon-user-focus-success" : ""} 
+                  ${status400 ? "icon-user-focus-error" : ""}`}
                   size={30}
                   weight="bold"
                 />
@@ -100,24 +152,36 @@ export const Form = () => {
               >
                 {showPassword ? (
                   <EyeSlash
-                    className={
+                    className={`
+                    ${
                       styleInputPassword ? "icon-user-focus" : "icon-user-blur"
-                    }
+                    } 
+                    ${status200 ? "icon-user-focus-success" : ""} 
+                    ${status400 ? "icon-user-focus-error" : ""}`}
                     size={30}
                     weight="bold"
                   />
                 ) : (
                   <Eye
-                    className={
+                    className={`
+                    ${
                       styleInputPassword ? "icon-user-focus" : "icon-user-blur"
-                    }
+                    } 
+                    ${status200 ? "icon-user-focus-success" : ""} 
+                    ${status400 ? "icon-user-focus-error" : ""}`}
                     size={30}
                     weight="bold"
                   />
                 )}
               </label>
             </div>
-            <button className="form-button">ENVIAR</button>
+            <button
+              className={`form-button 
+              ${status200 ? "button-focus-success" : ""} 
+              ${status400 ? "button-focus-error" : ""} `}
+            >
+              ENVIAR
+            </button>
           </form>
         </div>
       </div>
